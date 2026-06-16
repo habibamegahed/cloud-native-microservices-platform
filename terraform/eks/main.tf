fc781003b8bc035fb08e8211b1a3e438b5e2e98f
habibamegahed <155324916+habibamegahed@users.noreply.github.com>
2026-06-16 07:44:05 +0000

    fix

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -17,18 +17,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_iam_role" "ebs_csi_role" {
   name = "${var.project_name}-${var.environment}-ebs-csi-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
+  assume_role_policy = file("${path.module}/ebs-csi-trust-policy.json")
 
   tags = local.tags
 }
